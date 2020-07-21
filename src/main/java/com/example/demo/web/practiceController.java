c2e331d58ab29b076121443262f49bcf3a7dcb3b
ggomjae <ggomjae@naver.com>
2020-07-21 22:53:56 +0900

    react practice

--- src/main/java/com/example/demo/web/practiceController.java
+++ src/main/java/com/example/demo/web/practiceController.java
@@ -3,12 +3,22 @@ package com.example.demo.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class practiceController {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/index")
     public String testMethod(){
+        int y;
+        y = 3;
+
+        int x;
+        x = 3;
+        logger.info("하이욥" + x + y);
         return "index";
     }
 }
